3a9ef2a2e775f1435292eb1a57a2a05748c108c9
Aakarshit-Dhuria <aakarshitdhuria@gmail.com>
2023-05-05 14:20:30 +0530

    Added state to title.

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,35 +1,30 @@
 import './ExpenseItems.css';
 import ExpenseDate from './ExpenseDate.js';
 import Card from './Card';
+import { useState } from 'react';
 
 function ExpenseItems(props) {
 	// const expenseDate = new Date(2021, 2, 28);
 	// const expenseTitle = 'Car Insurance';
 	// const expenseAmount = 202.12;
+	// let title = props.title;
+
+	const [title, setTitle] = useState(props.title);
+
 	function clickHandler() {
+		setTitle('Updated Title');
 		console.log('Clicked!!!!!');
 	}
-	
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
 			<button onClick={clickHandler}>Change Title</button>
 		</Card>
-
-		//// Hardcoded way
-		// <div className="expense-item">
-		// 	<div>
-		// 		{expenseDate.toDateString().split(' ').splice(1).join(' ')}
-		// 	</div>
-		// 	<div className="expense-item__description">
-		// 		<h2>{expenseTitle}</h2>
-		// 		<div className="expense-item__price">${expenseAmount}</div>
-		// 	</div>
-		// </div>
 	);
 }
 
